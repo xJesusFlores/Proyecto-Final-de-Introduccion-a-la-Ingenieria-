393ddd3d66f729bf34e5ff40badb1e5f54d964df
saulhalgo <saulhidalgo422@aragon.unam.mx>
2021-12-13 13:36:12 -0600

    holaxdxd

---
+++ funcionSaul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int buscador(int item, int arr[], int numElementos)
+{
+	int lEncontrado = FALSO;
+	int i;
+	int posicion = -1;
+	
+	// busqueda secuencial
+	i = 0;
+	while (i < numElementos && !lEncontrado)
+	{
+		printf("Comparando %i %i\n", item, arr[i]);
+		if (item == arr[i])
+		{
+			lEncontrado = VERDADERO;
+			posicion = i;
+		}
+		i++;
+	}
+
+	return posicion;
