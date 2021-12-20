041a53f2b3d93d39a6aae19b84168596a98d6996
xJesusFlores <Jesusflores90@aragon.unam.mx>
2021-12-19 21:14:19 -0600

    Funciones implementadas en  proyecto principal

--- proyecto.c
+++ proyecto.c
@@ -10,11 +10,8 @@ printf("Ingrese el segundo valor\n");
 scanf_s("%d",&num2);
 resultado = num1 + num2;
 printf("El resultado de la suma es: %d\n",resultado);
-return 0;
 
-}
 
-{
 //funcion de que suma los numeros que hay desde cero hasta la cantidad deseada (Rey)
 
 	int limite;
@@ -32,10 +29,9 @@ return 0;
 	}
 
 	printf("La suma de los primeros %i numeros es %i", contador-1, suma);
-}
+
 //////////////////// FUNCION SAUL HIDALGO ARCOS /////////////////
 
-#include <stdio.h>
 
 #define NUMDATOS	5
 #define VERDADERO	1
@@ -62,4 +58,70 @@ int buscador(int item, int arr[], int numElementos)
 	}
 
 	return posicion;
-//
+
+//Funcionisai.cpp
+
+	int dia, mes, year;
+	scanf_s("%i", &dia);
+	scanf_s("%i", &mes);
+	scanf_s("%i", &year);
+
+	//dia = 28;
+	//mes = 2;
+	//year = 2021;
+	dia = dia + 1;
+	bool bi;
+	if (year % 4 == 0) {
+		if (year % 100 == 0) {
+			if (year % 400 == 0) {
+				bi = 1;
+			}
+			else {
+				bi = 0;
+			}
+		}
+		else {
+			bi = 1;
+		}
+	}
+	else {
+		bi = 0;
+	}
+
+
+	if (dia > 30) {
+		if (mes != 12) {
+			mes = mes + 1;
+			dia = 1;
+			printf("%2i %2i %4i", dia, mes, year);
+		}
+		else {
+			mes = 1;
+			dia = 1;
+			year = year + 1;
+			printf("%2i %2i %4i", dia, mes, year);
+		}
+	}
+	else {
+		if (mes == 2 && dia > 28) {
+			if (dia == 29) {
+				if (bi == 1) {
+					printf("%2i %2i %4i", dia, mes, year);
+				}
+				else {
+					mes = mes + 1;
+					dia = 1;
+					printf("%2i %2i %4i", dia, mes, year);
+				}
+			}
+			else {
+				mes = mes + 1;
+				dia = 1;
+				printf("%2i %2i %4i", dia, mes, year);
+			}
+		}
+		else {
+			printf("%2i %2i %4i", dia, mes, year);
+		}
+return 0;
+}
