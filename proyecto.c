607197c7460bd84978bf31e9506491014740ff62
saulhalgo <saulhidalgo422@aragon.unam.mx>
2021-12-19 19:59:06 -0600

    funcion del saulangas

--- proyecto.c
+++ proyecto.c
@@ -33,3 +33,33 @@ return 0;
 
 	printf("La suma de los primeros %i numeros es %i", contador-1, suma);
 }
+//////////////////// FUNCION SAUL HIDALGO ARCOS /////////////////
+
+#include <stdio.h>
+
+#define NUMDATOS	5
+#define VERDADERO	1
+#define FALSO		0
+
+
+int buscador(int item, int arr[], int numElementos)
+{
+	int lEncontrado = FALSO;
+	int i;
+	int posicion = -1;
+	
+	// busqueda secuencial
+	i = 0;
+	while (i < numElementos && !lEncontrado)
+	{
+		printf("Comparando %i %i\n", item, arr[i]);
+		if (item == arr[i])
+		{
+			lEncontrado = VERDADERO;
+			posicion = i;
+		}
+		i++;
+	}
+
+	return posicion;
+//
