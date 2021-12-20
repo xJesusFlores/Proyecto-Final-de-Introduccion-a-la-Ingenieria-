ecb71ec808eb36a9700bf120f4c79a3cfd6b5f34
MoisesMendozza <moises.mendoza2304@gmail.com>
2021-12-19 20:41:13 -0600

    Funcion del moy

---
+++ funcionMoises.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+//Funcion suma (Moises)
+{
+int num1, num2, resultado;
+printf("Ingrese el primer valor\n");
+scanf_s("%d", &num1);
+printf("Ingrese el segundo valor\n");
+scanf_s("%d",&num2);
+resultado = num1 + num2;
+printf("El resultado de la suma es: %d\n",resultado);
+return 0;
+
+}
